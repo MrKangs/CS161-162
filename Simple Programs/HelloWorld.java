aa5ac90b5bf25fd6158a9f2a163e9905ac9967fd
Mr.Kangs <gykang00@gmail.com>
2020-03-13 14:25:35 -0700

    Testing for Github with Ubuntu

---
+++ Simple Programs/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("How are you?"); // I'm not very good, How about you?
+	}
+}
